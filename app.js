27268f7d52f22ae0d0bb87762f2d2ce1bf4baf0f
Enéas da Silva Junior <35938994+EneasFeynlinus@users.noreply.github.com>
2022-06-30 12:31:42 -0300

    Upadate functions and variables.

--- app.js
+++ app.js
@@ -1,73 +1,48 @@
 const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
-const formSearch = document.querySelector('.form-search input')
+const inputFormSearch = document.querySelector('.form-search input')
+const clockContainer = document.querySelector('.clock-container')
 
-const addTodo = (inputValue) => {
+const addTodo = event => {
+    event.preventDefault()
+
+    const inputValue = event.target.add.value.trim()
 
     if (inputValue.length) {
         todosContainer.innerHTML += `
         <li class="list-group-item d-flex justify-content-between align-items-center" data-todos="${inputValue}">
         <span>${inputValue}</span>
         <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
+        </li>
         `
     }
+    event.target.reset()
 }
 
-formAddTodo.addEventListener('submit', event => {
-    event.preventDefault()
-    const inputValue = event.target.add.value
-    addTodo(inputValue)
-    event.target.reset()
-})
+const removeTodo = event => {
+    const isTrashValueClicked = event.target.dataset.trash
 
-const removeTodos = (clickedElement) => {
-    const trashValue = clickedElement.dataset.trash
-    const todo = document.querySelector(`[data-todos="${trashValue}"]`)
-    if (trashValue) {
+    if (isTrashValueClicked) {
+        const todo = document.querySelector(`[data-todos="${isTrashValueClicked}"]`)
         todo.remove()
     }
 }
 
-todosContainer.addEventListener('click', event => {
-    const clickedElement = event.target
-    removeTodos(clickedElement)
-})
-
-const filterTodos = (todos, inputValue, returnMatchedTodos) => todos
-    .filter(todos => {
-        const matchedTodos = todos.textContent.toLocaleLowerCase().includes(inputValue)
-        return returnMatchedTodos ? matchedTodos : !matchedTodos
-    })
-
-const manipulateClasses = (todos, classToAdd, classToRemove) => {
-    todos.forEach(todos => {
-        todos.classList.remove(classToRemove)
-        todos.classList.add(classToAdd)
-    })
-}
-
-
-const hideTodos = (todos, inputValue) => {
-    const totosToHide = filterTodos(todos, inputValue, false)
-    manipulateClasses(totosToHide, 'hidden', 'd-flex')
-
-}
-
-const showTodos = (todos, inputValue) => {
-    const todosToShow = filterTodos(todos, inputValue, true)
-    manipulateClasses(todosToShow, 'd-flex', 'hidden')
-}
-
-formSearch.addEventListener('input', event => {
-    const inputValue = event.target.value
+const searchTodo = event => {
+    const inputValue = event.target.value.toLowerCase()
     const todos = Array.from(todosContainer.children)
+        .map(todo => ({
+            todo,
+            shouldBeVisible: todo.textContent.toLowerCase().includes(inputValue)
+        }))
 
-    hideTodos(todos, inputValue)
-    showTodos(todos, inputValue)
+    todos.forEach(({ todo, shouldBeVisible }) => {
+        todo.classList.add(shouldBeVisible ? 'd-flex' : 'hide')
+        todo.classList.remove(shouldBeVisible ? 'hide' : 'd-flex')
+    })
 
-})
+}
 
-const clockContainer = document.querySelector('.clock-container')
 
 const clockUpdate = () => {
     const time = new Date()
@@ -84,3 +59,7 @@ const clockUpdate = () => {
 }
 
 setInterval(clockUpdate, 1000)
+
+formAddTodo.addEventListener('submit', addTodo)
+todosContainer.addEventListener('click', removeTodo)
+inputFormSearch.addEventListener('input', searchTodo)
